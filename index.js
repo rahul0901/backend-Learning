0c48ec72929980a45aa0d68316fa4d894eb74b6d
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-08 20:17:29 +0530

    addd

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import Express  from "express";
 import router from "./Routers/index.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import morgan from "morgan";
+import cors from 'cors';
 
 const app = Express();
+
+dotenv.config();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(Express.json())
 
 app.get('/', function(req, res){
     res.send('hello rahul')
 })
 
-app.use('/app/v1/', router)
+app.use('/app/v1/', router);
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('Database Connected'))
 
 app.listen(8000, ()=> console.log('running on port 8000'));
 
