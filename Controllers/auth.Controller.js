2bbcbc6e6a658a507b2afe71c117947e9a7fb79e
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-24 18:23:37 +0530

    adddd

--- Controllers/auth.Controller.js
+++ Controllers/auth.Controller.js
@@ -2,38 +2,69 @@ import UserModals from "../Modals/User.Modals.js";
 import bcrypt from 'bcrypt';
 
 export const Login = async (req, res) => {
-    try{
-        const {email, password} = req.body;
-        if(!email || !password)res.status(401).json({success:false, message: error})
 
-        const user  = await UserModals.find({email: email})
-        console.log(user, "user")
-        return res.send(true)
-    } 
-    catch(error){
-        return res.status(500).json({success:false, message: error})
+    try {
+
+        const { userKaEmail, userKaPassword } = req.body;
+
+        console.log(req.body)
+
+        if (!userKaEmail || !userKaPassword) {
+            return res.status(401).json({ success: false, message: 'All field required.' })
+        }
+
+        const user = await UserModals.findOne({ email: userKaEmail })
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'email not found' })
+        }
+
+        const isPassword = await bcrypt.compare(userKaPassword, user.userPassword)
+
+        if (!isPassword) {
+            return res.status(401).json({ success: false, message: 'pass not matched' })
+        }
+
+        return res.status(200).json({ success: true, message: 'Login Success' })
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'login error' })
     }
+    // try{
+    //     const {email, password} = req.body;
+    //     if(!email || !password)res.status(401).json({success:false, message: error})
+
+    //     const user  = await UserModals.find({email: email})
+    //     console.log(user, "user")
+    //     return res.send(true)
+    // } 
+    // catch(error){
+    //     return res.status(500).json({success:false, message: error})
+    // }
 }
 
 export const Register = async (req, res) => {
     try {
 
-        const{ name, email, password, number } = req.body;
+        const { userKaName, userKaEmail, userKaPassword, userKaNumber } = req.body;
+        // , userKaNumber
 
         console.log(req.body, "req.body")
 
-        if(!name || !email || !password || !number){
-            return res.status(401).json({success:'false', message:'all fields mandatory'})
+        if (!userKaName || !userKaEmail || !userKaPassword || !userKaNumber) {
+            return res.status(401).json({ success: 'false', message: 'all fields mandatory' })
         }
 
-        const hashPassword = await bcrypt.hash(password, 10);
-        // console.log(hashPassword)
+        // || !userKaNumber
+
+        const hashPassword = await bcrypt.hash(userKaPassword, 10);
+        console.log(hashPassword);
 
         const user = new UserModals({
-            name, 
-            email, 
+            name: userKaName,
+            email: userKaEmail,
             userPassword: hashPassword,
-            userNumber: number
+            userNumber: userKaNumber
         })
 
         await user.save();
@@ -45,6 +76,16 @@ export const Register = async (req, res) => {
     }
 }
 
-export const getCurrentUser = (req, res) => {
-    res.send("HII")
-}
+// export const getCurrentUser = (req, res) => {
+//     res.send("HII")
+// }
+
+
+
+  // "userKaEmail": "xyz@gmail.com",
+  // "userKaPassword": "xyz123"
+
+//   {
+//     "userKaEmail": "xyz1@gmail.com",
+//     "userKaPassword": "xyz1111"
+//   }
