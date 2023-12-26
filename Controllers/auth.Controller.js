9bac5d80638e31f92b1992b63e376b22f8c780c7
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-12-26 21:05:16 +0530

    adddd

--- Controllers/auth.Controller.js
+++ Controllers/auth.Controller.js
@@ -1,5 +1,6 @@
 import UserModals from "../Modals/User.Modals.js";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 export const Login = async (req, res) => {
 
@@ -24,23 +25,14 @@ export const Login = async (req, res) => {
         if (!isPassword) {
             return res.status(401).json({ success: false, message: 'pass not matched' })
         }
+// token id ka bana na hai toh id ayega sign ki baad
+        const token = await jwt.sign({id: user._id}, process.env.Jwt_Secret)
 
-        return res.status(200).json({ success: true, message: 'Login Success' })
+        return res.status(200).json({ success: true, message: 'Login Success', user: { userKaName: user.name, id: user.id }, token })
 
     } catch (error) {
         return res.status(500).json({ success: false, message: 'login error' })
     }
-    // try{
-    //     const {email, password} = req.body;
-    //     if(!email || !password)res.status(401).json({success:false, message: error})
-
-    //     const user  = await UserModals.find({email: email})
-    //     console.log(user, "user")
-    //     return res.send(true)
-    // } 
-    // catch(error){
-    //     return res.status(500).json({success:false, message: error})
-    // }
 }
 
 export const Register = async (req, res) => {
@@ -72,18 +64,38 @@ export const Register = async (req, res) => {
         return res.status(200).json({ success: 'true', message: 'Registration Successfull' })
 
     } catch (error) {
-        return res.status(500).json({ success: 'false', message: 'error' })
+        return res.status(500).json({ success: 'false', message: error })
     }
 }
 
-// export const getCurrentUser = (req, res) => {
-//     res.send("HII")
-// }
+export const getCurrentUser = async (req, res) => {
+    try {
+        
+        const {token} = req.body; 
+
+        if(!token) return res.status(401).json({success:false, message: 'token not found'})
+
+        const {id} = await jwt.verify(token, process.env.Jwt_Secret);
+
+        if(!id){
+            return res.status(401).json({success: false, message:'id required'})
+        }
+
+        const user = await UserModals.findById(id);
+
+        if(!user) return res.status(401).json({success:false, message:'user not found'})
+
+        return res.status(200).json({success: true, message:'User id Got success', user})
+
+    } catch (error) {
+        return res.status(500).json({success:false, message: error})
+    }
+}
 
 
 
-  // "userKaEmail": "xyz@gmail.com",
-  // "userKaPassword": "xyz123"
+// "userKaEmail": "xyz@gmail.com",
+// "userKaPassword": "xyz123"
 
 //   {
 //     "userKaEmail": "xyz1@gmail.com",
